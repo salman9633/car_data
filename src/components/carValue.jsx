d788507905daaed0934f7b9e557963c2492d6ddf
salmanfaris <salmanvaipees913@gmail.com>
2023-08-12 14:10:02 +0530

    total cost completed

--- src/components/carValue.jsx
+++ src/components/carValue.jsx
@@ -1,11 +1,28 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 
 function CarValue() {
+  // const totalCost = useSelector(({ cars: { carsData, searchTerm } }) => {
+  //   return carsData
+  //   .filter((car) => {
+  //     return car.name.toLowerCase().includes(searchTerm.toLowerCase())
+  //     // reduce((acc,car)=>{
+  //     //   return acc+car.cost
+  //     // },0)
+  //  })
+  // })
+
+  const totalCost = useSelector(({ cars: { carsData, searchTerm } }) => {
+    const filteredCars=carsData.filter((car) =>
+      car.name.toLowerCase().includes(searchTerm.toLowerCase()))
+    return filteredCars.reduce((acc,car)=>acc+car.cost,0)
+
+  })
   return (
-    <div>
-      CarValue
+    <div className='car-value'>
+      Total Cost: ${totalCost}
     </div>
   )
 }
- 
+
 export default CarValue
